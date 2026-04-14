f90c087ea134ee252ea6d5488a39916838ec837e
2222000271 <diasemillim2006@gmail.com>
2026-04-14 20:18:37 -0300

    insert dados

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner sc = new Scanner(System.in);
+    private static List<Cliente> clientes = new ArrayList<>();
+    private static int idCliente = 1;
+
     public static void main(String[] args) {
+        System.out.println("----------Cadastro do Cliente----------");
 
-        Cliente cliente = new Cliente( 1, "Patricia", "123.564.585-55", "Patricia_123@gmail.com", "teste123", "(47) 55662233", "Cliente", "Rua Otavio Linzmeier,22");
-        Entregador entregador = new Entregador(2,"Jose Maria","123.569.555-21","josemaria@gmail.com","123545", "36445587","Entregador",true);
+        System.out.println("Digite o nome do cliente");
+        String nome = sc.nextLine();
 
-                System.out.println("----------Cliente----------");
-                System.out.println(cliente.toString());
+        System.out.println("Digite o CPF do cliente");
+        String CPF = sc.nextLine();
 
-                System.out.println("----------Entregador----------");
-                System.out.println(entregador);
+        System.out.println("Digite o email do cliente");
+        String email = sc.nextLine();
+
+        System.out.println("Digite o senha do cliente");
+        String senha = sc.nextLine();
+
+        System.out.println("Digite o telefone do cliente");
+        String telefone = sc.nextLine();
+
+        System.out.println("Digite o endereço do cliente");
+        String endereco = sc.nextLine();
+
+        String tipoUsuario = "Cliente";
+
+        Cliente cliente = new Cliente(
+                idCliente,
+                nome,
+                CPF,
+                email,
+                senha,
+                telefone,
+                tipoUsuario,
+                endereco);
+
+        clientes.add(cliente);
+
+        idCliente++;
+
+        System.out.println("Cliente cadastrado com sucesso!!!");
+
+         System.out.println("Cliente: "+ cliente);
+
+
     }
-
 }
